459debe205da3e836c6d2c3bf88530f6d36ba830
Gonzalez Salvador <salvadorsopas@gmail.com>
2024-11-12 18:52:26 -0600

    implementando estructura de patron de diseño creacional Builder

--- Builder/src/com/gonzalez/pdd/Main.java
+++ Builder/src/com/gonzalez/pdd/Main.java
@@ -1,10 +1,22 @@
 package com.gonzalez.pdd;
 
 
+/*Separate the construction of a complex object from its 
+ * representation so that the same
+ * construction process can create
+ * different representation
+ * */
+
 public class Main {
 	public static void main(String[] args) {
+		// metodo principal del programa
+		// instanciamos el objeto de la clase
+		Usuario user = Usuario.make("Leo", "Maravillas")
+				.setEmail("leo@mail.com")
+				.setTelefono("55-11-18-30-20")
+				.setDireccion("Circuito Bicentenario cdmx 57,000")
+				.build();
 		
-		
+		System.out.println(user);
 	}
-
 }
--- Builder/src/com/gonzalez/pdd/Usuario.java
+++ Builder/src/com/gonzalez/pdd/Usuario.java
@@ -1,5 +1,65 @@
 package com.gonzalez.pdd;
 
 public class Usuario {
-
+	// generamos atributos privados de la clase
+	private String nombre;
+	private String apellido;
+	
+	private String email;
+	private String telefono;
+	private String direccion;
+	
+	//generamos un construcor privado que reciba los 2 parametros obligatorios, nombre, apellido
+	private Usuario(String nombre, String apellido){
+		this.nombre = nombre;
+		this.apellido = apellido;
+		
+		this.email = "";
+		this.telefono = "";
+		this.direccion = direccion;
+	}
+	
+	// generamos los setters de los objetos para poder tener acceso solamente mediante los mismos
+	// retornaran el objeto de la clase
+	public Usuario setNombre(String nombre) {
+		this.nombre = nombre;
+		return this;
+	}
+	public Usuario setApellido(String apellido) {
+		this.apellido = apellido;
+		return this;
+	}
+	public Usuario setEmail(String email) {
+		this.email = email;
+		return this;
+	}
+	public Usuario setTelefono(String telefono) {
+		this.telefono = telefono;
+		return this;
+	}
+	public Usuario setDireccion(String direccion) {
+		this.direccion = direccion;
+		return this;
+	}
+	
+	// generamos un metodo static que retorne el objeto de la clase
+	public static Usuario make(String nombre, String apellido) {
+		return new Usuario(nombre, apellido);
+	}
+	// generamos un metodo que reretorne todo el objeto de la clase
+	public Usuario build() {
+		return this;
+	}
+	
+	// rescribimos el metodo toString()
+	public String toString() {
+		return this.nombre + " " + this.apellido + " " + this.email + " " + this.telefono + " " + this.direccion;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
